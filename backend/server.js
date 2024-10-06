aab5abd865d320c441b8054797f679cd9c652973
Ashwin Gajbhiye <ashwingajbhiye@Ashwins-MacBook-Air.local>
2024-10-06 13:11:15 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDb from "./db/connectToMongoDb.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// app.get("/",(req,res)=>{
+//     res.send("hello world!!");
+// })
+
+app.use(express.json()); //to parse the incoming requests with json payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth" , authRoutes);
+app.use("/api/messages" , messageRoutes);
+app.use("/api/users" , userRoutes);
+
+app.listen(PORT,()=>{
+    connectToMongoDb();
+    console.log(`server is on port ${PORT}`);
+})
